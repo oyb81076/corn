a701c176daca2f1e56e42660e221d8357e6ddb4e
欧阳斌 <oyb@ouyangbindeiMac.local>
2019-07-09 10:40:10 +0800

    将位运算改为普通运算

--- packages/snowflake/__tests__/snowflake.spec.ts
+++ packages/snowflake/__tests__/snowflake.spec.ts
@@ -1,8 +1,8 @@
 import snowflake from "../internal/snowflake";
 it("create 5000", async () => {
-  const nextID = snowflake();
-  const count = 5000;
-  const array = await Promise.all(Array(count).fill(0).map(nextID))
+  const nextID = snowflake({ dataCenterId: 1, workerId: 1 });
+  const count = 1;
+  const array = await Promise.all(Array(count).fill(0).map(nextID));
   expect(new Set(array).size).toBe(count);
-  expect(array).toEqual(array.slice(0).sort())
-})
+  expect(array).toEqual(array.slice(0).sort());
+});
--- packages/snowflake/internal/snowflake.ts
+++ packages/snowflake/internal/snowflake.ts
@@ -1,4 +1,5 @@
-import long from "long";
+import { fromValue } from "long";
+
 interface IConfig {
   workerIdBits?: number;
   workerId?: number;
@@ -16,19 +17,7 @@ export default function snowflake({
   dataCenterIdBits = 5,
 }: IConfig = {}) {
   const sequenceBits = 22 - workerIdBits - dataCenterIdBits;
-  const maxWorkerId = -1 ^ (-1 << workerIdBits);
-  const maxDataCenterId = -1 ^ (-1 << dataCenterIdBits);
-  if (sequenceBits < 1) {
-    throw new Error("workerIdBits + dataCenterIdBits should low then 22");
-  }
-  if (workerId > maxWorkerId || workerId < 0) {
-    throw new Error('config.worker_id must max than 0 and small than maxWrokerId-[' +
-      maxWorkerId + ']');
-  }
-  if (dataCenterId > maxDataCenterId || dataCenterId < 0) {
-    throw new Error('config.data_center_id must max than 0 and small than maxDataCenterId-[' +
-      maxDataCenterId + ']');
-  }
+  validate(dataCenterIdBits, workerIdBits, sequenceBits, dataCenterId, workerId);
   return makeNextID(
     dataCenterIdBits,
     workerIdBits,
@@ -37,7 +26,29 @@ export default function snowflake({
     dataCenterId,
     workerId,
   );
-};
+}
+
+function validate(
+  dataCenterIdBits: number,
+  workerIdBits: number,
+  sequenceBits: number,
+  dataCenterId: number,
+  workerId: number,
+) {
+  const maxWorkerId = -1 ^ (-1 << workerIdBits);
+  const maxDataCenterId = -1 ^ (-1 << dataCenterIdBits);
+  if (sequenceBits < 1) {
+    throw new Error("workerIdBits + dataCenterIdBits should low then 22");
+  }
+  if (workerId > maxWorkerId || workerId < 0) {
+    throw new Error("workerId must max than 0 and less than maxWrokerId-[" +
+      maxWorkerId + "]");
+  }
+  if (dataCenterId > maxDataCenterId || dataCenterId < 0) {
+    throw new Error("dataCenterId must max than 0 and less than maxDataCenterId-[" +
+      maxDataCenterId + "]");
+  }
+}
 
 function makeNextID(
   dataCenterIdBits: number,
@@ -47,17 +58,12 @@ function makeNextID(
   dataCenterId: number,
   workerId: number,
 ): () => Promise<string> {
-  const workerIdShift = sequenceBits;
-  const dataCenterIdShift = sequenceBits + workerIdBits;
   const timestampLeftShift = sequenceBits + workerIdBits + dataCenterIdBits;
   const sequenceMask = -1 ^ (-1 << sequenceBits);
+  const dataCenterWorkerValue = dataCenterWorker(workerIdBits, sequenceBits, dataCenterId, workerId);
   let sequence = 0;
   let lastTimestamp = -1;
-  async function sleep(): Promise<number> {
-    return new Promise((r) => {
-      setTimeout(() => r(Date.now()), 1);
-    })
-  }
+
   return async function nextID() {
     let timestamp = Date.now();
     if (lastTimestamp === timestamp) {
@@ -71,18 +77,51 @@ function makeNextID(
       sequence = 0;
     }
     if (timestamp < lastTimestamp) {
-      throw new Error('Clock moved backwards. Refusing to generate id for ' +
+      throw new Error("Clock moved backwards. Refusing to generate id for " +
         (lastTimestamp - timestamp));
     }
 
     lastTimestamp = timestamp;
-    const centerWorkerSequenceNum = (dataCenterId << dataCenterIdShift) |
-      (workerId << workerIdShift) | sequence;
-    return long
-      .fromValue(timestamp - twepoch)
-      .shiftLeft(timestampLeftShift)
-      .or(centerWorkerSequenceNum)
-      .toString(10);
-  }
+    return format(
+      timestampLeftShift,
+      twepoch,
+      timestamp,
+      dataCenterWorkerValue,
+      sequence,
+    );
+  };
 }
 
+async function sleep(): Promise<number> {
+  return new Promise((r) => {
+    setTimeout(() => r(Date.now()), 1);
+  });
+}
+
+function dataCenterWorker(
+  workerIdBits: number,
+  sequenceBits: number,
+  dataCenterId: number,
+  workerId: number,
+) {
+  const workerIdShift = sequenceBits;
+  const dataCenterIdShift = sequenceBits + workerIdBits;
+  const dataCenterValue = dataCenterId << dataCenterIdShift;
+  const workerValue = workerId << workerIdShift;
+  const dataCenterWorkerValue = dataCenterValue + workerValue;
+  return dataCenterWorkerValue;
+}
+
+function format(
+  timestampLeftShift: number,
+  twepoch: number,
+  timestamp: number,
+  dataCenterWorkerValue: number,
+  sequence: number,
+) {
+  const leftValue = timestamp - twepoch;
+  const rightValue = dataCenterWorkerValue + sequence;
+  // js的位运算不如 + - 运算, 所以我把位运算都改成了+ - 运算了
+  const value = fromValue(leftValue).shiftLeft(timestampLeftShift).add(rightValue);
+  return value.toString(10);
+}
